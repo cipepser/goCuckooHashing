693e89d0bd219cf7985df5b620ff69a8e0591f46
cipepser <respepic@gmail.com>
2017-05-05 19:07:09 +0900

    reformat CuckooHashing.go

--- src/CuckooHashing.go
+++ src/CuckooHashing.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"math/big"
 )
 
 const (
-	N int64 = 10 // テーブルの大きさ
-	MAXLOOP int = 100
+	N       int64 = 10 // テーブルの大きさ
+	MAXLOOP int   = 100
 )
 
 func hash(key int64) (h1, h2 int64) {
 	hasher := md5.New()
 	hasher.Write([]byte(string(key)))
-	
+
 	h := hex.EncodeToString(hasher.Sum(nil))
 
 	h1_t, _ := new(big.Int).SetString(h[:int(len(h)/2)], 16)
@@ -36,7 +36,7 @@ func NewCuckoo() *Cuckoo {
 
 func (c *Cuckoo) lookup(key int64) bool {
 	h1, h2 := hash(key)
-	
+
 	if c.T1[h1] == key || c.T2[h2] == key {
 		return true
 	} else {
@@ -44,7 +44,7 @@ func (c *Cuckoo) lookup(key int64) bool {
 	}
 }
 
-func (c *Cuckoo) insert(key int64, cnt int) {	
+func (c *Cuckoo) insert(key int64, cnt int) {
 	for cnt < MAXLOOP {
 		h1, h2 := hash(key)
 		switch cnt % 2 {
@@ -61,20 +61,20 @@ func (c *Cuckoo) insert(key int64, cnt int) {
 				return
 			} else {
 				key, c.T2[h2] = c.T2[h2], key
-			}	
+			}
 		}
 		cnt++
 	}
 	panic("fail to insert. you must reconstruct the Table...")
-}	
+}
 
 func (c *Cuckoo) delete(key int64) {
 	h1, h2 := hash(key)
-	
+
 	if c.T1[h1] == key {
 		c.T1[h1] = 0
 	}
-	
+
 	if c.T2[h2] == key {
 		c.T2[h2] = 0
 	}
@@ -83,14 +83,14 @@ func (c *Cuckoo) delete(key int64) {
 
 func main() {
 	c := NewCuckoo()
-	
+
 	// insert the keys.
 	cnt := 0
 	x := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	for _, key := range x {
 		c.insert(key, cnt)
 	}
-	
+
 	// look up for the key "1" and "10".
 	fmt.Println("key:1  ", c.lookup(1))  // key:1   true
 	fmt.Println("key:10 ", c.lookup(10)) // key:10  false
